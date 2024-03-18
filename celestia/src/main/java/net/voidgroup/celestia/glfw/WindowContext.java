f8965e524a34a0f3a1c0e38832b4546b32e01876
CelDaemon <devoid@voidgroup.net>
2024-03-18 01:13:00 +0100

    Improve code readability

--- celestia/src/main/java/net/voidgroup/celestia/glfw/WindowContext.java
+++ celestia/src/main/java/net/voidgroup/celestia/glfw/WindowContext.java
@@ -23,11 +23,10 @@ public class WindowContext implements AutoCloseable {
     }
 
     public void clearColor(float r, float g, float b, float a) {
-        System.out.println(STR."\{r},\{g},\{b},\{a}");
         glLibrary.glClearColor.execute(r, g, b, a);
         glLibrary.glClear.execute(GLLibrary.GL_COLOR_BUFFER_BIT);
     }
     public void clearColor(byte r, byte g, byte b, byte a) {
-        clearColor(1f / 256f * (float) (r & 0xFF), 1f / 256f * (float) (g & 0xFF), 1f / 256f * (float) (b & 0xFF), 1f / 256f * (float) (a & 0xFF));
+        clearColor(1f / 256 * Byte.toUnsignedInt(r), 1f / 256 * Byte.toUnsignedInt(g), 1f / 256 * Byte.toUnsignedInt(b), 1f / 256 * Byte.toUnsignedInt(a));
     }
 }
